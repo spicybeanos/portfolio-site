288790c48511848527ea6bf6164613da74fc51b6
Aryan D <aryan.d.dalal@gmail.com>
2025-04-23 01:58:11 +0530

    removed es lint

--- src/components/IExperience.tsx
+++ src/components/IExperience.tsx
@@ -1,5 +1,5 @@
 import ApexExperience from './ApexExperience';
-import { Experience } from '../types';
+import { Experience } from '@/types';
 
 //rounded-tr-xl rounded-bl-xl
 //{ title, tags, url, description }: { title: string, tags: string, url?: string, description: string }
